4dfd6ce7dd216e5cf85632b7f2c08e91e392f8ad
cerebrium <nicholas.m.shankland@gmail.com>
2019-12-31 10:06:30 -0800

    initial commit

---
+++ client/src/pages/account.js
@@ -0,0 +1,33 @@
+// src/pages/account.js
+import React from "react"
+import { login, isAuthenticated, getProfile } from "../utils/auth"
+import { Link } from "gatsby"
+import axios from 'axios'
+
+const Home = ({ user }) => {
+  return <p>Hi, {user.name ? user.name : "friend"}!</p>
+}
+const Settings = () => <p>Settings</p>
+const Billing = () => <p>Billing</p>
+
+const Account = () => {
+  if (!isAuthenticated()) {
+    login()
+    return <p>Redirecting to login...</p>
+  }
+
+  const user = getProfile()
+
+  return (
+    <div>
+      <nav className="navBar">
+        <Link to="/account">home</Link>{' | '}
+        <Link to="/foods">Places</Link>{' | '}
+        <Link to="/places">Foods</Link>{' | '}
+      </nav>
+      <h1>Travel Foodie Express</h1>
+    </div>
+  )
+}
+
+export default Account
